2dd84bdc8b704edb3abd8a43117ca70bf1a2bd67
Serraniel <online-daniel95@web.de>
2017-01-04 19:09:20 +0100

    Added configuration class

---
+++ DOB AutoRole/Core/Configuration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DOB_AutoRole.Core
+{
+    public class Configuration
+    {
+        public int Id { get; set; }
+        public string Token { get; set; }
+        public string FriendlyName { get; set; }
+    }
+}
