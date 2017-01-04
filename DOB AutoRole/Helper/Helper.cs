ced5d8e2fbf611b9ea863e6b8f31d540b8cc4104
Serraniel <online-daniel95@web.de>
2017-01-04 18:58:22 +0100

    Static helper with help function for AppData folder

---
+++ DOB AutoRole/Helper/Helper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DOB_AutoRole.Helper
+{
+    internal static class Helper
+    {
+        internal static string WorkingDirectory
+        {
+            get
+            {
+                var tempPath = Environment.GetEnvironmentVariable("appdata") + @"\Nep Bot";
+                var d = new DirectoryInfo(tempPath);
+
+                tempPath = d.FullName;
+
+                if (!tempPath.EndsWith("\\"))
+                {
+                    tempPath = string.Concat(tempPath, "\\");
+                }
+
+                if (!d.Exists)
+                {
+                    Debug("Creating working directory...");
+                    Directory.CreateDirectory(tempPath);
+                }
+
+
+                return tempPath;
+            }
+        }
+    }
+}
